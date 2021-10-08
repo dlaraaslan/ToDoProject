35eaa3238c570b63ffbf3adc83bb01754db48aed
DILARA ASLAN <daslan17@posta.pau.edu.tr>
2021-10-08 16:01:36 +0300

    Entity layer classes created

---
+++ Entity/Concrete/Task.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Task: IEntity
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
